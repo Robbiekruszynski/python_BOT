8d6257f44f634bd0196fa2568ec7e66405c8e179
robbie <robbiekruszynski@gmail.com>
2020-03-28 17:00:06 -0700

    add greeting and hello test in bot.py

--- bot.py
+++ bot.py
@@ -1,20 +1,29 @@
 import discord
-import config
+from discord.ext import commands
+# import config
+Bot = commands.Bot(command_prefix=" . ")
+# client = discord.Client()
+@Bot.event
+async def on_ready():
+    print(' bot is ready ')
 
-client = discord.Client()
-@client.event
+
+@Bot.event
+async def on_memeber_join(memeber):
+    print(f'(memeber) has joined the One Million Developer Party!')
+
+
+@Bot.event
 async def on_message(message):
     message.content.lower()
-    if message.author == client.user:
+    if message.author == Bot.user:
         return
-    if message.content.startswith("hello?"):
+    if message.content.startswith("hello"):
         await message.channel.send("Hello, I've been waiting for you...")
-    if message.content.startswith("what?"):
-        await message.channel.send("That's right, I've been watching you...")
-    if message.content.startswith("test"):
-        await message.channel.send("Test running")
+#     if message.content.startswith("what?"):
+#         await message.channel.send("That's right, I've been watching you...")
+#     if message.content.startswith("test"):
+#         await message.channel.send("Test running")
 
 
-client.run(config.token)
-# connection =S3Connection(config.token)
-
+Bot.run('')
