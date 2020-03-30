f802729104010e02e0fc8ea72fa67bb4181329cc
robbie <robbiekruszynski@gmail.com>
2020-03-30 09:38:19 -0700

    add logic to greet new user via DM

--- bot.py
+++ bot.py
@@ -1,30 +1,60 @@
 import discord
 from discord.ext import commands
-client = commands.Bot(command_prefix = '!')
+
+
+TOKEN = ""
+client = discord.Client()
+
+
+client = commands.Bot(command_prefix='!')
+
+
+@client.command()
+async def ping(ctx):
+    await ctx.send('pong')
+
+
+@client.command()
+async def add(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
+
+
+#     @client.event
+#     async def on_member_join(self, member):
+#         guild = member.guild
+#         if guild.system_channel is not None:
+#             to_send = 'Welcome {0.mention} to {1.name}! Have a look around and if you have any questions feel free to reach out to ADMINS'.format(
+#                 member, guild)
+#             await guild.system_channel.send(to_send)
+# #
 
 @client.event
 async def on_ready():
-    print('BOT, RISE')
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+
+@client.event
+async def on_message(message):
+    message.content = message.content.lower()
+    if message.author == client.user:
+        return
+    if message.content.startswith("running?"):
+        await message.channel.send("test bot running")
 
 
 @client.event
 async def on_member_join(member):
-    print(f'{member} has joined the Bridges!')
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to the One Million Developer discord!'
+    )
 
-@client.command()
-async def ping(ctx):
-    await ctx.send('Pong!')
+# @client.command()
+# async def foo(ctx):
+#     await ctx.send('Hello')
 
-@client.event
-async def on_message(message):
-    message.content.lower()
-    if message.author == client.user:
-        return
-    if message.content.startswith("hello"):
-        await message.channel.send("Hello, I've been waiting for you...")
-    if message.content.startswith("what?"):
-        await message.channel.send("That's right, I've been watching you...")
-    if message.content.startswith("test"):
-        await message.channel.send("Test running")
-
-client.run('')
+client.run(TOKEN)
