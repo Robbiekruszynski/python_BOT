647214fcea94662e0baa827ee8a24ac074c1dff6
robbie <robbiekruszynski@gmail.com>
2020-04-01 14:27:24 -0700

    update

--- bot.py
+++ bot.py
@@ -1,60 +1,40 @@
 import discord
 from discord.ext import commands
-
+import random
 
 TOKEN = ""
-client = discord.Client()
 
+bot = commands.Bot(command_prefix='!')
 
-client = commands.Bot(command_prefix='!')
-
-
-@client.command()
-async def ping(ctx):
-    await ctx.send('pong')
-
-
-@client.command()
-async def add(ctx, left: int, right: int):
-    """Adds two numbers together."""
-    await ctx.send(left + right)
-
-
-#     @client.event
-#     async def on_member_join(self, member):
-#         guild = member.guild
-#         if guild.system_channel is not None:
-#             to_send = 'Welcome {0.mention} to {1.name}! Have a look around and if you have any questions feel free to reach out to ADMINS'.format(
-#                 member, guild)
-#             await guild.system_channel.send(to_send)
-# #
-
-@client.event
+@bot.event
 async def on_ready():
     print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+@bot.event
+async def on_member_join(member):
+    guild = member.guild
+    arrival = ["Hi", "Hello", "Welcome"]
+    greeting = random.choice(arrival)
+    if guild.system_channel is not None:
+        to_send = f'{greeting}' '{0.mention} to {1.name}! if you have any question please feel free to reach out to an Admin'.format(member, guild)
+        await guild.system_channel.send(to_send)
+
+
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('Pong!')
+
+
+
+async def on_ready(self):
+    print('Logged in as')
+    print(self.user.name)
+    print(self.user.id)
     print('------')
 
 
-@client.event
-async def on_message(message):
-    message.content = message.content.lower()
-    if message.author == client.user:
-        return
-    if message.content.startswith("running?"):
-        await message.channel.send("test bot running")
-
-
-@client.event
-async def on_member_join(member):
-    await member.create_dm()
-    await member.dm_channel.send(
-        f'Hi {member.name}, welcome to the One Million Developer discord!'
-    )
-
-# @client.command()
-# async def foo(ctx):
-#     await ctx.send('Hello')
-
-client.run(TOKEN)
+bot.run(TOKEN)
