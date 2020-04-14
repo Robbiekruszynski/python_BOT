6b9ed0b51c7c40faeb272b0e0b7d0932d7553983
robbie <robbiekruszynski@gmail.com>
2020-04-14 07:33:23 -0700

    add logic for welcome section and delete members.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 import random
-TOKEN = "NjkyOTA4MjQ1NDg3OTc2NTA5.XoI50A.-CRu6AH5sJ8Sc2l2oBOfcAxiULw"
+TOKEN = ""
 
 bot = commands.Bot(command_prefix='!')
 
@@ -14,18 +14,19 @@ async def on_ready():
     print('------')
 
 
+
 @bot.event
 async def on_member_join(member):
     guild = member.guild
     arrival = ["Hi", "Hello", "Welcome"]
     greeting = random.choice(arrival)
+
     if guild.system_channel is not None:
-        to_send = f'{greeting}' '{0.mention} to {1.name}! if you have any question please feel free to reach out to an Admin'.format(
-            member, guild)
-        await guild.system_channel.send(to_send)
-
-
-
+        to_send ='{2} {0.mention} to {1.name}! if you have any question please feel free to reach out to an Admin'.format(
+            member, guild, greeting)
+        welcome = list(filter(lambda x: x.name == 'welcome', guild.channels))[0]
+        print(guild.channels)
+        await welcome.send(to_send)
 @bot.command()
 async def ping(ctx):
     await ctx.send('Pong!')
@@ -37,7 +38,7 @@ async def add(ctx, left: int, right: int):
 
 @bot.command()
 async def lost(ctx):
-    await ctx.send('This is a dedicated place to share efforts within the OMD community, take a look at the channels that look interesting to you! Always feel free to ping an Admin you see on the right column')
+    await ctx.send('This is a dedicated place to share efforts within the OMD community, take a look at the channels on the left column and check out any that look interesting to you! Always feel free to ping an Admin you see on the right column')
 @bot.command()
 async def joined(ctx, member: discord.Member):
     await ctx.send('{0.name} joined in {0.joined_at}'.format(member))
