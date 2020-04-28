d4d2c2557d91d100f87b2dddb37a631657586226
robbie <robbiekruszynski@gmail.com>
2020-04-28 12:55:03 -0700

    update bot for welcome tab

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 import random
-TOKEN = "TOKEN"
+TOKEN = ""
 
 bot = commands.Bot(command_prefix='!')
 
@@ -14,42 +14,29 @@ async def on_ready():
     print('------')
 
 
-
 @bot.event
 async def on_member_join(member):
     guild = member.guild
-    arrival = ["Hi", "Hello", "Welcome"]
+    arrival = ["Hi", "Hello", "Hey"]
     greeting = random.choice(arrival)
 
     if guild.system_channel is not None:
-        to_send ='{2} {0.mention} to {1.name}! if you have any question please feel free to reach out to an Admin'.format(
+        to_send ='{2} {0.mention}, welcome to Teku! \nNew here? Introduce yourself in #introductions. \nTeku is an open source, eth2.0 client, written in Java created by the PegaSys team. \nUseful links: \nGithub: https://github.com/pegasyseng/ \ntekuDocs: https://docs.teku.pegasys.tech/en/latest/ ' \
+                 '\nPegasys website: http://pegasys.tech/teku'.format(
             member, guild, greeting)
         welcome = list(filter(lambda x: x.name == 'welcome', guild.channels))[0]
-        print(guild.channels)
         await welcome.send(to_send)
-@bot.command()
-async def ping(ctx):
-    await ctx.send('Pong!')
-
 
 @bot.command()
 async def add(ctx, left: int, right: int):
     await ctx.send(left + right)
 
 @bot.command()
+async def ping(ctx):
+    await ctx.send("Pong")
+
+@bot.command()
 async def lost(ctx):
     await ctx.send('This is a dedicated place to share efforts within the OMD community, take a look at the channels on the left column and check out any that look interesting to you! Always feel free to ping an Admin you see on the right column')
-@bot.command()
-async def joined(ctx, member: discord.Member):
-    await ctx.send('{0.name} joined in {0.joined_at}'.format(member))
-
-
-@bot.command()
-async def cool(ctx):
-    await ctx.send('Not cool')
-
-
-
-
 
 bot.run(TOKEN)
